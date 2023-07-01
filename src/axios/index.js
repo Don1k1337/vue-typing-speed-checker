7d6d858d1b5d44c5d4ebaaf73adac9c973a5ed54
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-07-01 10:30:19 +0600

    feat: Unnecessary instance headers removed.

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 10000,
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    },
 });
 
 export default instance;
