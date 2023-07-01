b03916616e548f6bb7b928cce0440285ee18e935
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-07-01 09:38:33 +0600

    feat: another proxy added.

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_CORS_PROXY + import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.VITE_HOST_URL,
     timeout: 30000,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Access-Control-Allow-Origin': import.meta.env.VITE_CORS_URL
+
     },
 });
 
